a1428b3a12f830bb7f0c01d018cb0e7530b040a7
Rene Jean Corneille <corneille.r.j@gmail.com>
2018-08-01 05:21:34 +0100

    execution script change

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ SOURCE="${BASH_SOURCE[0]}"
 
 DIR="$( cd -P "$( dirname "$SOURCE")" && pwd )/extracts/"
 
-g++ src/svm.cc -lopencv_core -lopencv_ml -lopencv_imgproc -o src/svm.out
+g++ -std=c++17 src/svm.cc -lopencv_core -lopencv_ml -lopencv_imgproc -o src/svm.out
 
 src/svm.out $DIR
